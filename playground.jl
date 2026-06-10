75aa19439d0ddd4d250c9a819ea18d1cf62f5bbb
Jeff Groves <jeffrey.groves@mail.com>
2026-06-10 07:28:32 -0600

    initial commit

---
+++ playground.jl
@@ -0,0 +1,9 @@
+
+# To use: cut and paste section of interest; each section is actually commented out so you can't load this file directly
+
+#= TIMESTAMP 20260610.072430
+
+
+
+
+TIMESTAMP 20260610.072430 =#
