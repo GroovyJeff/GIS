ef917f487f67f761c40841f2929d28bab06d454a
Jeff Groves <jeffrey.groves@mail.com>
2026-06-10 07:44:50 -0600

    checkpoint

--- playground.jl
+++ playground.jl
@@ -3,6 +3,32 @@
 
 #= TIMESTAMP 20260610.072430
 
+using GeoDataFrames
+using ZipFile
+using ArchGDAL
+
+# let's create a function takes a shapefile name and returns a matrix indicating category of each pixel
+
+# let's test with Canadian provinces using the /vsizip protocol
+
+# these are the input variables
+
+s = "/vsizip/GADM/gadm41_CAN_shp.zip/gadm41_CAN_shp.zip/gadm41_CAN_1.shp"
+h = 8192
+w = 16384
+
+# load the data using GeoDataFrames
+
+my_data = GeoDataFrames.read(s)
+
+
+
+
+
+
+# I assume you've downloaded gadm.org shapefiles to GADM
+
+@time myMasks = Rasters.boolmask(myData; to = myExt, size=(my_w, my_h), collapse = false, boundary = :touches);
 
 
 
